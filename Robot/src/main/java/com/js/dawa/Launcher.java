305fa5c1584fa24870cc5e4a4e0207e3058a9cbb
jsalle <jsalle@0232APPT>
2025-03-21 12:06:50 +0100

    add

--- Robot/src/main/java/com/js/dawa/Launcher.java
+++ Robot/src/main/java/com/js/dawa/Launcher.java
@@ -1,5 +1,33 @@
 package com.js.dawa;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.js.dawa.iu.arene.Arene;
+import com.js.dawa.iu.arene.CaseArene;
+import com.js.dawa.iu.arene.render.CaseAreneRenderDefaut;
+import com.js.dawa.iu.console.Console;
+import com.js.dawa.iu.console.ConsoleGraphique;
+
+
 public class Launcher {
+	
+	
+	
+	public static void main(String[] args) {
+		Console lConsole = new ConsoleGraphique();
+		Map<String, String> lParams = new HashMap<String, String>();
+		lParams.put("titre", "Arene");
+		lConsole.init(lParams);
+		Arene lArene = new Arene(lConsole);
+		
+		CaseArene lCaseArene = new CaseArene();
+		lCaseArene.setCaseAreneRender(new CaseAreneRenderDefaut());
+		
+		lArene.addCaseArene(10, 10, lCaseArene);
+		
+		
+		lArene.printArene();
+	}
 
 }
